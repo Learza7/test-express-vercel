de74a1d8ab980a0c0b8849043e3261ac6fc69df7
Learza7 <remystryx@gmail.com>
2023-06-10 01:59:46 +0200

    added prisma

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // index.js
 const express = require('express')
 
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 const app = express()
 
 
@@ -11,6 +15,11 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  });
 
 // Export the Express API
 module.exports = app
